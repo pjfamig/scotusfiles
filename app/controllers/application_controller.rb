ef9593b89a1799cdb785740422019cd65c4028e3
pjfamig <p0seid0n_@hotmail.com>
2023-02-04 20:33:25 -0800

    Add admin to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   before_action do
     ActiveStorage::Current.host = 'http://localhost:3000'
   end
+  
+  protected 
+  
+  def authenticate_admin!
+    authenticate_user!
+    redirect_to root_path, status: :forbidden unless current_user.admin?
+  end
+  
 end
